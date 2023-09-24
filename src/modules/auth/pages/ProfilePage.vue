551d0d793d6244e8eb3579e16ee1cc63672876ed
kien <trgkien.vu@gmail.com>
2023-09-24 11:29:03 +0700

    feat: initiate routing

---
+++ src/modules/auth/pages/ProfilePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Profile page</h1>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'master',
+            component: () => import('@/modules/master/MasterModule.vue')
+        },
+        {
+            path: '/auth',
+            name: 'auth',
+            component: () => import('@/modules/auth/AuthModule.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'profile',
+                    alias: 'profile',
+                    component: () => import('@/modules/auth/pages/ProfilePage.vue')
+                },
+                {
+                    path: 'login',
+                    name: 'login',
+                    component: () => import('@/modules/auth/pages/LoginPage.vue')
+                }
+            ]
+        },
+        {
+            path: '/gallery',
+            name: 'gallery',
+            component: () => import('@/modules/gallery/pages/GalleryPage.vue')
+        }
+    ]
 })
 
 export default router
