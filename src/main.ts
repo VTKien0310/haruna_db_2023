bc870eb8d83e890b789f44cc8cf27df004f2f48f
kien <trgkien.vu@gmail.com>
2023-09-24 10:35:05 +0700

    installed Vuestic UI

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import {createVuestic} from "vuestic-ui";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createVuestic())
 
 app.mount('#app')
