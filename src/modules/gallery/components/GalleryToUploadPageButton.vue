53909c12312c9bcf9b9a1a2d839ac02941f219c6
kien <trgkien.vu@gmail.com>
2023-10-07 11:13:26 +0700

    feat: added go to gallery upload page

--- src/modules/gallery/components/GalleryToUploadPageButton.vue
+++ src/modules/gallery/components/GalleryToUploadPageButton.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import router from "@/router";
+import {GalleryRouteName} from "@/modules/gallery/GalleryRouter";
 
+function goToGalleryUploadPage(): void {
+  router.push({name: GalleryRouteName.UPLOAD})
+}
 </script>
 
 <template>
-<va-button
-    round
-    size="large"
-    icon="upload"
-/>
+  <va-button
+      round
+      size="large"
+      icon="upload"
+      @click="goToGalleryUploadPage"
+  />
 </template>
 
 <style scoped>
