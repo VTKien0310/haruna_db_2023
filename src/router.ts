f148b16780e4cadbfa29079253f00e43afb0cf84
kien <trgkien.vu@gmail.com>
2023-11-05 10:46:04 +0700

    feat: updated router scroll behavior saving

--- src/router.ts
+++ src/router.ts
@@ -14,7 +14,7 @@ const router = createRouter({
         if (savedPosition) {
             return savedPosition
         } else {
-            return {top: 0}
+            return { left: 0, top: 0}
         }
     },
 })
