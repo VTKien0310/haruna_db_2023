4d19d511e7333cae6f98cd42825f5110cea5d5c8
kien <trgkien.vu@gmail.com>
2024-01-07 10:30:13 +0700

    Refactor GalleryListPage and improve scroll functionality
    Renamed 'galleryContainer' to 'galleryListPageContent' and implemented a new function called 'loadMoreMedias'. This function sets a condition to fetch more media when user scrolls past 75% of the page height. Also, enhanced the UI specifically in the media fetch progress indicator section.

--- src/modules/gallery/pages/GalleryListPage.vue
+++ src/modules/gallery/pages/GalleryListPage.vue
@@ -7,21 +7,35 @@ import GalleryListItem from "@/modules/gallery/components/GalleryListItem.vue";
 
 const galleryListStore = useGalleryListStore();
 
-const galleryContainer = ref<HTMLDivElement | null>(null);
+const galleryListPageContent = ref<HTMLDivElement | null>(null);
 
 const saveGalleryContainerLastScrollPosition = (): void => {
-  galleryListStore.galleryContainerScrollPosition = galleryContainer.value?.scrollTop ?? 0
+  galleryListStore.galleryContainerScrollPosition = galleryListPageContent.value?.scrollTop ?? 0
 }
 
 const restoreGalleryContainerLastScrollPosition = (): void => {
-  if (galleryContainer.value) {
-    galleryContainer.value.scrollTop = galleryListStore.galleryContainerScrollPosition;
+  if (galleryListPageContent.value) {
+    galleryListPageContent.value.scrollTop = galleryListStore.galleryContainerScrollPosition;
   }
 }
+
+interface ScrollEventDataType {
+  target: {
+    scrollTop: number,
+    clientHeight: number,
+    scrollHeight: number
+  }
+}
+
+const loadMoreMedias = ({target: {scrollTop, clientHeight, scrollHeight}}: ScrollEventDataType) => {
+  if (scrollTop + clientHeight >= scrollHeight * 0.75) {
+    galleryListStore.fetchMedias()
+  }
+}
+
 onActivated(() => {
   restoreGalleryContainerLastScrollPosition()
 })
-
 
 onMounted(() => {
   galleryListStore.refreshMedias()
@@ -29,28 +43,36 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="min-h-screen">
+  <div
+      @scroll="loadMoreMedias"
+      @scrollend="saveGalleryContainerLastScrollPosition"
+      ref="galleryListPageContent"
+      class="h-screen overflow-scroll"
+  >
 
+    <div class="grid grid-cols-4 gap-px place-content-center place-items-center">
+      <GalleryListItem
+          v-for="media in galleryListStore.medias"
+          :key="media.id"
+          :media="media"
+      />
+    </div>
+
+    <!-- medias fetch progress indicator -->
     <div
-        ref="galleryContainer"
-        @scrollend="saveGalleryContainerLastScrollPosition"
-        class="max-h-screen overflow-hidden"
+        v-show="galleryListStore.isFetchingGalleryMedias"
+        class="w-full flex flex-row justify-center content-center items-center"
     >
-      <va-infinite-scroll :load="galleryListStore.fetchMedias">
-        <div
-            class="grid grid-cols-4 gap-px place-content-center place-items-center">
-          <GalleryListItem
-              v-for="media in galleryListStore.medias"
-              :key="media.id"
-              :media="media"
-          />
-        </div>
-      </va-infinite-scroll>
+      <va-progress-circle indeterminate/>
     </div>
 
     <div class="fixed bottom-12 right-3">
       <GalleryToUploadPageButton/>
     </div>
+
+    <!-- push the content up so the app bar won't block the view -->
+    <div class="h-20"></div>
+
   </div>
 </template>
 
