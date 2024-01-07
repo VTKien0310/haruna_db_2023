816a83cbef51053f66f6e86faaa1ed9a9eae46ef
kien <trgkien.vu@gmail.com>
2024-01-07 17:30:42 +0700

    Refactor routing in App.vue
    Removed useRouter and useIonRouter dependencies from App.vue and replaced these with a direct import of the router object. This change was implemented to simplify the code and improve the app's performance.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import {useRouter} from 'vue-router'
 import {computed} from "vue";
 import {AuthRouteName} from "@/modules/auth/AuthRouter";
 import {MasterRouteName} from "@/modules/master/MasterRouter";
 import {GalleryRouteName} from "@/modules/gallery/GalleryRouter";
 import {useAuthStore} from "@/modules/auth/stores/AuthStore";
-import {IonApp, IonRouterOutlet, useIonRouter} from '@ionic/vue';
+import {IonApp, IonRouterOutlet} from '@ionic/vue';
+import router from "@/router";
 
 const authStore = useAuthStore()
 authStore.registerOnAuthStateChange()
 
-const ionRouter = useIonRouter()
-
-const router = useRouter()
 const hideNavBar = computed((): boolean => {
   return router.currentRoute.value.name === AuthRouteName.LOGIN
 })
@@ -30,19 +27,19 @@ const hideNavBar = computed((): boolean => {
         class="flex flex-row justify-around content-center items-center"
     >
       <va-button
-          @click="ionRouter.push({name: MasterRouteName.MASTER})"
+          @click="router.push({name: MasterRouteName.MASTER})"
           icon="home"
           color="backgroundPrimary"
           preset="secondary"
       />
       <va-button
-          @click="ionRouter.push({name: GalleryRouteName.LIST})"
+          @click="router.push({name: GalleryRouteName.LIST})"
           icon="image"
           color="backgroundPrimary"
           preset="secondary"
       />
       <va-button
-          @click="ionRouter.push({name: AuthRouteName.PROFILE})"
+          @click="router.push({name: AuthRouteName.PROFILE})"
           icon="person"
           color="backgroundPrimary"
           preset="secondary"
