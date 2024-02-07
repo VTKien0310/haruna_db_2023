32f643966a179811ce96f4635382ccc372d8f962
KonKeRoR <trgkien.vu@gmail.com>
2024-02-07 17:11:43 +0700

    feat: Add media count and upload contribution to MasterPage
    The changes introduced the countTotalMedias and countUserUploadedMedias methods in the GalleryListStore. These methods are used in the MasterPage.vue to calculate the total number of media and the number of media uploaded by the user, respectively. These values are then used to show the user's upload contribution percentage.

--- src/modules/gallery/stores/GalleryListStore.ts
+++ src/modules/gallery/stores/GalleryListStore.ts
@@ -3,6 +3,8 @@ import {ref} from "vue";
 import type {Media} from "@/modules/gallery/GalleryEntities";
 import {supabasePort} from "@/ports/supabase/SupabasePort";
 import {useToast} from "vuestic-ui";
+import {data} from 'autoprefixer';
+import type {User} from '@supabase/supabase-js';
 
 export const useGalleryListStore = defineStore('gallery-list', () => {
     const isFetchingGalleryMedias = ref<boolean>(false);
@@ -71,12 +73,55 @@ export const useGalleryListStore = defineStore('gallery-list', () => {
         return fetchMedias()
     }
 
+    async function countTotalMedias(): Promise<number> {
+        const { count, error } = await supabasePort.
+            from('medias').
+            select('*', {
+                count: 'exact',
+                head: true,
+            });
+
+        if (error || count === null) {
+            init({
+                message: 'Failed to count total medias',
+                color: 'danger',
+            });
+
+            return 0;
+        }
+
+        return count;
+    }
+
+    async function countUserUploadedMedias(user: User): Promise<number> {
+        const {count, error} = await supabasePort.
+            from('medias').
+            select('*', {
+                count: 'exact',
+                head: true,
+            }).
+            eq('uploader_id', user.id);
+
+        if (error || count === null) {
+            init({
+                message: "Failed to count user's uploaded medias",
+                color: 'danger',
+            });
+
+            return 0;
+        }
+
+        return count;
+    }
+
     return {
         isFetchingGalleryMedias,
         hasFetchedAllRecords,
         medias,
         fetchMedias,
         refreshMedias,
-        reset
+        reset,
+        countTotalMedias,
+        countUserUploadedMedias,
     }
 })
--- src/modules/master/pages/MasterPage.vue
+++ src/modules/master/pages/MasterPage.vue
@@ -1,11 +1,53 @@
 <script setup lang="ts">
-import {IonPage} from "@ionic/vue";
+import {IonPage, onIonViewDidEnter} from '@ionic/vue';
+import {useGalleryListStore} from '@/modules/gallery/stores/GalleryListStore';
+import {useAuthStore} from '@/modules/auth/stores/AuthStore';
+import {computed, ref} from 'vue';
+
+const galleryListStore = useGalleryListStore();
+const authStore = useAuthStore();
+
+const totalMediasCount = ref<number>(0);
+const uploadedMediasCount = ref<number>(0);
+
+const uploadContributionPercentage = computed(() => {
+  // create a filling up animation instead of a draining down for the progress bar
+  if (uploadedMediasCount.value === 0 || totalMediasCount.value === 0) {
+    return 0;
+  }
+
+  return Math.round((uploadedMediasCount.value * 100) / totalMediasCount.value);
+});
+
+onIonViewDidEnter(async () => {
+  const me = await authStore.me();
+
+  uploadedMediasCount.value = await galleryListStore.countUserUploadedMedias(me!);
+  totalMediasCount.value = await galleryListStore.countTotalMedias();
+});
 </script>
 
 <template>
   <ion-page>
-    <div class="w-full min-h-screen flex flex-col justify-center items-center content-center">
-      <h1 class="title-font">Work in progress...</h1>
+    <div class="w-full min-h-screen flex flex-col justify-start items-center content-center">
+      <div class="w-full">
+        <h5>Total media count: {{ totalMediasCount }}</h5>
+        <h5>Uploaded media count: {{ uploadedMediasCount }}</h5>
+      </div>
+
+      <div class="w-full">
+        <h5>Upload contribution percentage</h5>
+        <va-progress-bar
+            :model-value="uploadContributionPercentage"
+            :max="100"
+            size="large"
+            content-inside
+            show-percent
+        >
+          {{ uploadContributionPercentage }}
+        </va-progress-bar>
+      </div>
+
     </div>
   </ion-page>
 </template>
